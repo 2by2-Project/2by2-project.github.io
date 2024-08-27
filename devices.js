393bd6e13c87d80a2e1a44a7d984e96cd5c724b3
Sheelq <151790051+Sheelq@users.noreply.github.com>
2024-08-27 16:20:35 -0300

    Update devices.js

--- devices.js
+++ devices.js
@@ -1,32 +1,55 @@
-let devices = null;
+let devices = [];
 
 window.onload = async () => {
-    devices = await getDevices();
+    try {
+        devices = await getDevices();
+        const path = location.href;
 
-    // Run init script
-    if (location.href.includes('devices.htm')) initDevicesPage();
-    if (location.href.includes('download.htm')) initDownloadPage();
+        if (path.includes('devices.htm')) {
+            await initDevicesPage();
+        } else if (path.includes('download.htm')) {
+            await initDownloadPage();
+        }
+    } catch (error) {
+        console.error('Failed to initialize:', error);
+    }
 }
 
 const initDevicesPage = async () => {
     const devicesContainer = document.querySelector('#devices');
+    const bannerDisplay = document.querySelector('#device_banner');
+    const iconDisplay = document.querySelector('#device_icon');
+
     if (!devicesContainer) return;
 
-    for (let i = 0; i < devices.length; i++) {
-        var deviceDownloadLinkLi = document.createElement('li');
-
-        var deviceDownloadLink = document.createElement('a');
-        deviceDownloadLink.innerText = devices[i].name;
-        deviceDownloadLink.href = `./devices/${devices[i].codename}`;
-
-        var deviceDownloadCodename = document.createElement('code');
-        deviceDownloadCodename.innerText = devices[i].codename;
-        deviceDownloadCodename.style.marginLeft = '32px';
-        
-        deviceDownloadLinkLi.appendChild(deviceDownloadLink);
-        deviceDownloadLinkLi.appendChild(deviceDownloadCodename);
-        
+    devices.forEach(device => {
+        const deviceDownloadLinkLi = createDeviceListItem(device);
         devicesContainer.appendChild(deviceDownloadLinkLi);
+    });
+
+    displayDeviceBannerAndIcon(bannerDisplay, iconDisplay, devices[0]);
+}
+
+const createDeviceListItem = (device) => {
+    const deviceDownloadLinkLi = document.createElement('li');
+    const deviceDownloadLink = document.createElement('a');
+    deviceDownloadLink.innerText = device.name;
+    deviceDownloadLink.href = `./download.htm?device=${device.codename}`;
+
+    const deviceDownloadCodename = document.createElement('code');
+    deviceDownloadCodename.innerText = device.codename;
+    deviceDownloadCodename.style.marginLeft = '32px';
+
+    deviceDownloadLinkLi.append(deviceDownloadLink, deviceDownloadCodename);
+    return deviceDownloadLinkLi;
+}
+
+const displayDeviceBannerAndIcon = (bannerDisplay, iconDisplay, device) => {
+    if (bannerDisplay) {
+        bannerDisplay.innerText = device.banner || 'No banner available';
+    }
+    if (iconDisplay) {
+        iconDisplay.innerHTML = device.icon ? `<img src="${device.icon}" alt="${device.name} icon">` : 'No icon available';
     }
 }
 
@@ -38,18 +61,58 @@ const initDownloadPage = async () => {
     const changeLogsDisplay = document.querySelector('#device_changelog');
     const downloadLink = document.querySelector('#device_download_link');
     const recoveryLinkContainer = document.querySelector('#device_recovery_links');
+    const bannerDisplay = document.querySelector('#device_banner');
+    const iconDisplay = document.querySelector('#device_icon');
+
     if (!codenameDisplay || !downloadLink || !recoveryLinkContainer) return;
 
-    const params = new URLSearchParams(window.location.search);
-    const targetDevice = params.get('device');
-    const device = devices.find((device) => device.codename === targetDevice);
+    const targetDevice = new URLSearchParams(window.location.search).get('device');
+    const device = devices.find(d => d.codename === targetDevice);
+
     if (!device) {
-        nameDisplay.innerText = 'Error: No such device found: ' + targetDevice;
+        nameDisplay.innerText = `Error: No such device found: ${targetDevice}`;
         return;
     }
 
-    const otaDetails = await getOtaDetails(device.codename);
+    try {
+        const [otaDetails, changeLogs] = await Promise.all([
+            getOtaDetails(device.codename),
+            fetch(`https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/changelogs/${device.codename}.txt`).then(response => response.text())
+        ]);
 
+        updateDownloadPageContent({
+            nameDisplay,
+            codenameDisplay,
+            maintainerDisplay,
+            warningDisplay,
+            changeLogsDisplay,
+            downloadLink,
+            recoveryLinkContainer,
+            device,
+            otaDetails,
+            changeLogs,
+            bannerDisplay,
+            iconDisplay
+        });
+    } catch (error) {
+        console.error('Failed to initialize download page:', error);
+    }
+}
+
+const updateDownloadPageContent = ({ 
+    nameDisplay, 
+    codenameDisplay, 
+    maintainerDisplay, 
+    warningDisplay, 
+    changeLogsDisplay, 
+    downloadLink, 
+    recoveryLinkContainer, 
+    device, 
+    otaDetails, 
+    changeLogs, 
+    bannerDisplay, 
+    iconDisplay 
+}) => {
     nameDisplay.innerText = device.name;
     codenameDisplay.innerText = device.codename;
     maintainerDisplay.innerText = device.maintainer;
@@ -57,36 +120,29 @@ const initDownloadPage = async () => {
     if (device.copy_partitions) {
         warningDisplay.style.display = 'block';
         warningDisplay.innerHTML = `For first flashing, you need to flash copy-partitions.zip with OTA zips.`
-                                 + `<a href="https://sourceforge.net/projects/project2by2-test/files/misc/copy_partitions/copy-partitions-20220613-signed.zip/download">Download is here</a>`;
+                                    + `<a href="https://sourceforge.net/projects/project2by2-test/files/misc/copy_partitions/copy-partitions-20220613-signed.zip/download">Download is here</a>`;
     }
 
-    const changeLogs = await fetch(`https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/changelogs/${device.codename}.txt`).then(response => response.text());
     changeLogsDisplay.innerText = changeLogs;
 
     downloadLink.innerText = otaDetails.filename;
     downloadLink.href = otaDetails.url;
 
-    let separator = '';
-    device.recovery_images.forEach((partition) => {
-        var recoveryLink = document.createElement('a');
-        recoveryLink.innerText = `${partition}.img`;
-        recoveryLink.href = `https://master.dl.sourceforge.net/project/project2by2-test/${device.codename}/${otaDetails.platform_version}/${partition}/${partition}.img?viasf=1`;
-        recoveryLinkContainer.appendChild(document.createTextNode(separator));
-        recoveryLinkContainer.appendChild(recoveryLink);
-        separator = ' , ';
-    });
+    recoveryLinkContainer.innerHTML = device.recovery_images.map(partition => 
+        `<a href="https://master.dl.sourceforge.net/project/project2by2-test/${device.codename}/${otaDetails.platform_version}/${partition}/${partition}.img?viasf=1">${partition}.img</a>`
+    ).join(' , ');
+
+    displayDeviceBannerAndIcon(bannerDisplay, iconDisplay, device);
 }
 
 const getDevices = async () => {
-    const devicesJsonUrl = 'https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/devices.json';
-    const fetchData = await fetch(devicesJsonUrl);
-    const jsonData = await fetchData.json();
-    return jsonData.devices;
+    const response = await fetch('https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/devices.json');
+    const data = await response.json();
+    return data.devices;
 }
 
 const getOtaDetails = async (codename) => {
-    const devicesJsonUrl = `https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/builds/${codename}.json`;
-    const fetchData = await fetch(devicesJsonUrl);
-    const jsonData = await fetchData.json();
-    return jsonData.response[0];
+    const response = await fetch(`https://raw.githubusercontent.com/2by2-Project/android_vendor_2by2-ota/main/builds/${codename}.json`);
+    const data = await response.json();
+    return data.response[0];
 }
